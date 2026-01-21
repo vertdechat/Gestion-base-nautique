fc42753f652c0fc723d4df216c26ff1bd46b2f01
vertdechat <56255619+vertdechat@users.noreply.github.com>
2026-01-21 10:55:43 +0100

    Delete Python312 directory

--- Python312/Lib/site-packages/pyad/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import absolute_import
-from .adbase import set_defaults as pyad_setdefaults
-__all__ = ["adbase", "adquery", "adsearch", "adobject", "adcomputer", "adcontainer", "addomain", "adgroup", "aduser", "pyad"]
